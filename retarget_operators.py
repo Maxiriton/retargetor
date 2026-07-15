b1c595cb76fc11104d8f37147cbd4384caad0d9c
Henri Hebeisen <henri.hebeisen@gmail.com>
2026-07-15 22:57:27 +0200

    Progress on bone matching compute

--- retarget_operators.py
+++ retarget_operators.py
@@ -10,6 +10,24 @@ from bpy.props import StringProperty
 
 from mathutils import Matrix, Quaternion, Vector
 
+def compute_bone_offset(context, bone_source_name, bone_target_name):
+    """Calculate the bone """
+
+    rot_src_edit_local = context.scene.retarget_source_armature.data.bones[bone_source_name].matrix_local.copy().to_3x3()
+    rot_tgt_edit_local = context.scene.retarget_target_armature.data.bones[bone_target_name].matrix_local.copy().to_3x3()
+
+    x_src = rot_src_edit_local @ Vector((1.0,0.0,0.0))
+
+    x_src_in_tgt_local = rot_tgt_edit_local.inverted() @ x_src
+
+    return math.atan2(x_src_in_tgt_local.z, x_src_in_tgt_local.x)
+
+def compute_rest_offset(context, bone_source_name, bone_target_name):
+    """Get the rotation difference between the two bones"""
+    rot_src = context.scene.retarget_source_armature.data.bones[bone_source_name].matrix_local.copy().to_quaternion()
+    rot_tgt = context.scene.retarget_target_armature.data.bones[bone_target_name].matrix_local.copy().to_quaternion()
+    return rot_tgt.rotation_difference(rot_src)
+
 
 def callback_update_lerp_property(self, context):
     lerp_value = self.lerp_rotation
@@ -33,53 +51,6 @@ def find_bone_in_target(context, target_bone_name):
             return bone.name
     return ""
 
-
-def add_empties_at_target_bones(context):
-    """Add empties at the location of target bones for visualization"""
-    target_armature = context.scene.retarget_target_armature
-    src_armature = context.scene.retarget_source_armature
-    scene = context.scene
-    if not target_armature or not src_armature:
-        return
-    
-    for obj in bpy.data.objects:
-        if obj.name.startswith("Empty_"):
-            bpy.data.objects.remove(obj, do_unlink=True)
-
-    for bone_name, bone_info in target_armature['bone_mapping'].items():
-        bone_tgt = target_armature.pose.bones.get(bone_name)
-
-        if bone_tgt:
-            empty_name = f"Empty_{bone_name}"
-            empty = bpy.data.objects.new(empty_name, None)
-            context.collection.objects.link(empty)
-            empty.location = target_armature.matrix_world @ bone_tgt.head
-            empty.empty_display_size = 0.03
-            empty.empty_display_type = 'ARROWS'
-            empty.rotation_mode = 'QUATERNION'
-
-            const = empty.constraints.new('COPY_ROTATION')
-            const.target = context.scene.retarget_source_armature
-            const.subtarget = bone_info.get('src_bone', "")
-
-            const_loc = empty.constraints.new('COPY_LOCATION')
-            const_loc.target = context.scene.retarget_target_armature
-            const_loc.subtarget = bone_tgt.name
-
-
-def init_bone_mapping_from_json(context, json_path):
-    """Initializes the bone_tgt mapping from a JSON file and store bone_tgt mapping in a dict as a custom property of the target armature"""
-
-    if not os.path.exists(json_path):
-        raise FileNotFoundError(f"JSON file not found: {json_path}")
-
-    bone_mapping = {}
-    with open(json_path, 'r', encoding='utf-8') as f:
-        for item in json.load(f):
-            if item.get("en", False) and "src" in item and "tgt" in item:
-                bone_mapping[item["tgt"]] = {"src_bone": item["src"]}
-
-    context.scene.retarget_target_armature['bone_mapping'] = bone_mapping
 
 def fill_bone_mapping_offset(context, source_armature, target_armature):
     """Fill the bone_tgt mapping with rotation offsets based on the rest pose of source and target armatures"""
@@ -124,13 +95,13 @@ class RTGTR_Setup_Bone_List(Operator):
             #we try to find the matching bones in target_armature (based on names only)
             print(f"on va chercher la target pour {bone.name}")
             rtg_item.target_name = find_bone_in_target(context, bone.name)
-
+            if rtg_item.target_name: 
+                rtg_item.roll_offset = compute_bone_offset(context, rtg_item.src_name, rtg_item.target_name)
+                rtg_item.rest_offset = compute_rest_offset(context, rtg_item.src_name, rtg_item.target_name)
             
 
         self.report({'INFO'}, f"Found {len(scene.retarget_bones)} bones")
         return {'FINISHED'}      
-
-
 
 
 class RTG_SimpleRetarget(Operator):
